bc0073c1506e241811a82fdd7e99c3b5aea4ead8
Khánh hào <khanhao1903@gmail.com>
2020-08-12 01:08:39 +0700

    luan van v3

--- index.js
+++ index.js
@@ -5,5 +5,5 @@
 import { AppRegistry } from 'react-native';
 import Run from 'thitracnghiem/Navigation/Export';
 import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => Run);
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
+AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(Run));
